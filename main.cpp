15a727f8fa4a8d48a7dfc4d0e6b847d8efdc3760
erhuisinga <60323199+erhuisinga@users.noreply.github.com>
2020-04-15 20:20:52 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,19 @@
+//initial main.cpp by Ben Breshears, edited by Trey Nebury and Emily Huisinga
+
+#include <iostream>
+#include <string>
+#include <array>
+#include "DayOfYear.h"
+using namespace std;
+
+
+
+int main() {
+
+    int test1;
+    DayOfYear test = 57;
+    test.print();
+    cin >> test1;
+
+    return 0;
+}
